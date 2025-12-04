a475237bf43169fb209d5fca16969d0b8fdd6285
Niru200374 <nirutasilwal21@gmail.com>
2025-12-04 10:33:07 +0545

    task 1,2,3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    // Method that accepts an array and a condition
+    static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (int n in numbers)
+        {
+            if (condition(n))
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 12, 18, 7 };
+
+        Console.WriteLine("Even numbers:");
+        ProcessNumbers(nums, n => n % 2 == 0);
+
+        Console.WriteLine("\nNumbers > 10:");
+        ProcessNumbers(nums, n => n > 10);
+    }
+}
