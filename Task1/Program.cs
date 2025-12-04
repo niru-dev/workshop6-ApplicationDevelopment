16a798c0a1685fe0c506de14f7fb8fb66aa3ab98
Niru200374 <nirutasilwal21@gmail.com>
2025-12-04 10:21:42 +0545

    task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle(5, 3);
+            rect.PrintArea();
+            rect.PrintPerimeter();
+        }
+
+    }
+}
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        // Expression-bodied read-only properties
+        public double Area => Length * Width;
+        public double Perimeter => 2 * (Length + Width);
+
+        // Expression-bodied constructor
+        public Rectangle(double length, double width) =>
+            (Length, Width) = (length, width);
+
+        // Expression-bodied methods
+        public void PrintArea() => Console.WriteLine($"Area: {Area}");
+        public void PrintPerimeter() => Console.WriteLine($"Perimeter: {Perimeter}");
+    }
+}
